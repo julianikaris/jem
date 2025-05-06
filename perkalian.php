b005beb393412a7d6b57d3d3e79b5c671ced10b2
julianikaris <julianiekaristi19@gmail.com>
2025-05-07 04:38:38 +0700

    Update perkalian.php

--- perkalian.php
+++ perkalian.php
@@ -1,1 +1,43 @@
 http://localhost/perkalian.php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tabel Perkalian 10x10</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 50%;
+        }
+        th, td {
+            border: 1px solid black;
+            padding: 10px;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+
+<table>
+    <tr>
+        <th>&times;</th>
+        <?php
+        for ($i = 1; $i <= 10; $i++) {
+            echo "<th>$i</th>";
+        }
+        ?>
+    </tr>
+
+    <?php
+    for ($i = 1; $i <= 10; $i++) {
+        echo "<tr>";
+        echo "<th>$i</th>"; // Kolom pertama menunjukkan angka pertama
+        for ($j = 1; $j <= 10; $j++) {
+            echo "<td>" . ($i * $j) . "</td>";
+        }
+        echo "</tr>";
+    }
+    ?>
+</table>
+
+</body>
+</html>
